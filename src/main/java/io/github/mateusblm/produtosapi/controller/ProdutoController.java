993ac87820fcb3a1fc562582dd2c8b16a559842d
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-20 15:59:35 -0300

    Criando métodos de deletar produtos, atualizar produtos e buscar com parametros

--- src/main/java/io/github/mateusblm/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/mateusblm/produtosapi/controller/ProdutoController.java
@@ -2,11 +2,10 @@ package io.github.mateusblm.produtosapi.controller;
 
 import io.github.mateusblm.produtosapi.model.Produto;
 import io.github.mateusblm.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 //Classe da API
@@ -20,13 +19,40 @@ public class ProdutoController {
         this.produtoRepository = produtoRepository;
     }
 
-    @PostMapping
+    @PostMapping  //Operação para salvar produto
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto recebido: " + produto);
+
         var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        Optional<Produto> byId = produtoRepository.findById(id);
+        return byId.orElse(null);
+
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id,
+                          @RequestBody Produto produto) {
         produto.setId(id);
         produtoRepository.save(produto);
 
-        return produto;
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){ //, @RequestParam("preco") Double preco)
+        // return produtoRepository.findByPreco(preco);
+        return produtoRepository.findByNome(nome);
     }
 }
--- src/main/java/io/github/mateusblm/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/io/github/mateusblm/produtosapi/repository/ProdutoRepository.java
@@ -4,6 +4,13 @@ package io.github.mateusblm.produtosapi.repository;
 import io.github.mateusblm.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.security.SecureRandom;
+import java.util.List;
+
 // Classe que encapsula as operações de persistência na classe produto
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+    List<Produto> findByPreco(Double preco);
+
 }
